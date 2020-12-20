698d3136b3f4b2e2cc2f02b4da13b72bda4f7fb3
Philthy-Phil <phil.ziegelbauer@gmx.at>
2020-12-20 08:14:32 +0100

    quick-setup

--- src/AOC_2020/aoc_19_01.java
+++ src/AOC_2020/aoc_19_01.java
@@ -1,4 +1,31 @@
 package AOC_2020;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class aoc_19_01 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("inputs/input_18_01_test.txt"));
+        ArrayList<String> allInputLines = new ArrayList<>();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+
+            String lineMod = line.replace(" ", "");
+            allInputLines.add(lineMod);
+        }
+        br.close();
+
+
+
+
+
+
+    }
+
+
 }
--- src/AOC_2020/aoc_20_01.java
+++ src/AOC_2020/aoc_20_01.java
@@ -1,4 +1,28 @@
 package AOC_2020;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class aoc_20_01 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("inputs/input_test.txt"));
+        ArrayList<String> allInputLines = new ArrayList<>();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+
+            allInputLines.add(line);
+        }
+        br.close();
+
+
+
+
+
+    }
+
 }
